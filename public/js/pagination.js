df2b6a2a812df3ec265d7eb2c3313d96888fa316
calin1611 <calin1611@gmail.com>
2016-03-11 12:17:24 +0200

    Pagination done

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -6,19 +6,19 @@ $('#pagination').on('click', 'a', function(e) { // When click on a 'a' element o
 	var page = this.id; // Page number is the id of the 'a' element
 	var pagination = ''; // Init pagination
 
-	$('#articleArea').html('<img src="design/loader-small.gif" alt="" />'); // Display a processing icon
+	// $('#articleArea').html('<img src="design/loader-small.gif" alt="" />'); // Display a processing icon
 	var data = {page: page, per_page: 4}; // Create JSON which will be sent via Ajax
 	// We set up the per_page var at 4. You may change to any number you need.
 
 	$.ajax({ // jQuery Ajax
-		type: 'POST',
-		url: 'ajax/tuto-pagination.php', // URL to the PHP file which will insert new value in the database
+		method: 'POST',
+		url: 'http://localhost/blog/articles/paginate', // URL to the PHP file which will insert new value in the database
 		data: data, // We send the data string
 		dataType: 'json', // Json format
 		timeout: 3000,
 		success: function(data) {
 			$('#articleArea').html(data.articleList); // We update the articleArea DIV with the article list
-			
+
 			// Pagination system
 			if (page == 1) pagination += '<div class="cell_disabled"><span>First</span></div><div class="cell_disabled"><span>Previous</span></div>';
 			else pagination += '<div class="cell"><a href="#" id="1">First</a></div><div class="cell"><a href="#" id="' + (page - 1) + '">Previous</span></a></div>';
