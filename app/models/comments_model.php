e6580b0921792b89bd94dd935ac7b3b6782297e2
calin1611 <calin1611@gmail.com>
2016-03-15 16:42:35 +0200

    Comments working

--- app/models/comments_model.php
+++ app/models/comments_model.php
@@ -1,13 +1,19 @@
 <?php
-    require_once "db_model.php";
+  require_once "db_model.php";
 
-    class CommentsModel extends DB{
-        function getAll() {
-            $statement = $this->executeQuery("SELECT * FROM comments");
-            return $statement->fetchAll(PDO::FETCH_ASSOC);
-        }
-
-        function addComment($comment) {
-            $this->executeQuery("INSERT INTO comments (article_id, email, body) VALUES ('" . $comment["article_id"] . "', '" . $comment["email"] . "', '" . $comment["body"] . "');");
-        }
+  class CommentsModel extends DB{
+    function getAll() {
+      $statement = $this->executeQuery("SELECT * FROM comments");
+      return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    function getComments($articleId){
+      $statement = $this->executeQuery("SELECT * FROM comments WHERE article_id = " . $articleId);
+      return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function addComment($comment) {
+      $this->executeQuery("INSERT INTO comments (article_id, user, title, body) VALUES ('" . $comment["article_id"] . "', '" . $comment["user"] . "', '" . $comment["title"] . "', '" . $comment["body"] . "');");
+      return $this->db->lastInsertId();
+    }
+  }
--- app/views/article_view.php
+++ app/views/article_view.php
@@ -7,3 +7,27 @@
   Published on:
   <?php echo $articleCreationDate; ?>
 </p>
+
+<h3>Comments:</h3>
+  <?php
+  // var_dump($comments);
+    foreach ($comments as $key => $value) {
+      echo "<div class='comment-wrapper'>";
+
+      echo"<h5>".$value["user"]." says: </h5>";
+      echo"<h4>".$value["title"]."</h4>";
+      echo "<p>". $value["body"] . "</p>";
+      echo "<p>Date: " . $value["creation_date"] . "</p>";
+
+      echo "</div>";
+    }
+  ?>
+
+  <div class="comment-form">
+    <form action="http://localhost/blog/article/postComment?id=<?php echo $_GET['id']; ?>" method="post">
+      <input type="text" class="form-control" name="name" placeholder="Your name..."/>
+      <input type="text" class="form-control" name="title" placeholder="Title" />
+      <textarea class="form-control" name="body" rows="8" cols="40" placeholder="Your comment"></textarea>
+      <input type="submit" class="btn btn-primary" name="submit" value="Post comment">
+    </form>
+  </div>
