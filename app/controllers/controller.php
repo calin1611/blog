7cbf9f3ad7794aa64b24ddc631e9bdb40366fdb5
calin1611 <calin1611@gmail.com>
2016-03-15 14:27:43 +0200

    Cleanup

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -1,58 +1,51 @@
 <?php
-    class Controller {
-        function __construct() {
-            //toate paginile disponibile in blog
-            $pages = array(
-                "articles" => array("path" => "articles.php", "class" => "Articles"),
-                "article" => array("path" => "article.php", "class" => "Article"),
-                "contact" => array("path" => "contact.php", "class" => "Contact"),
-                "login" => array("path" => "login.php", "class" => "Login"),
-                "admin" => array("path" => "admin.php", "class" => "Admin")
-            );
+  class Controller {
+    function __construct() {
+      //toate paginile disponibile in blog
+      $pages = array(
+        "articles" => array("path" => "articles.php", "class" => "Articles"),
+        "article" => array("path" => "article.php", "class" => "Article"),
+        "contact" => array("path" => "contact.php", "class" => "Contact"),
+        "login" => array("path" => "login.php", "class" => "Login"),
+        "admin" => array("path" => "admin.php", "class" => "Admin")
+      );
 
-            //structura: localhost/rootFolder/controller/method
-            //blog/articles/add
-            //daca e doar blog/articles => indexul controllerului "articles"
-            //daca e blog/articles/add e accesata metoda "add"
+      //structura: localhost/rootFolder/controller/method
+      //blog/articles/add
+      //daca e doar blog/articles => indexul controllerului "articles"
+      //daca e blog/articles/add e accesata metoda "add"
 
-            // print_r( pathinfo($_SERVER["REQUEST_URI"]));
-            // var_dump($_GET);
+      $uri = $_SERVER["PATH_INFO"];
+      // $uri = $_SERVER["REQUEST_URI"];
+      $segments = explode("/", $uri);
+      //segments array: [0]=>"" [1]=>root [2]=>controller [3]=>method
+      //pagina default pentru aterizare (landing page) cand acceseaza rootul aplicatiei
 
-            $uri = $_SERVER["PATH_INFO"];
-            // $uri = $_SERVER["REQUEST_URI"];
-            $segments = explode("/", $uri);
-            //segments array: [0]=>"" [1]=>root [2]=>controller [3]=>method
-// var_dump($_SERVER);
-// var_dump($segments);
-// var_dump($uri);
-            //pagina default pentru aterizare (landing page) cand acceseaza rootul aplicatiei
-            $page = "articles";
-            $controller = $segments[1];
-            $method = empty($segments[2]) ? "" : $segments[2];
-            if(!empty($controller) && $controller != "index.php") {
-                if (array_key_exists($controller, $pages)) {
-                    $page = $controller;
-                }
-                else {
-                    http_response_code(404);
-                    var_dump($segments);
-                    echo "<br>" . $_SERVER["REQUEST_URI"];
-
-                    echo "<br> NOT FOUND";
-                    var_dump($_GET);
-                    exit;
-                }
-            }
-            // echo "controller var:";
-            // print_r(pathinfo($_SERVER["REDIRECT_URL"]));
-
-            require $pages[$page]["path"];
-            $controllerClass = new $pages[$page]["class"]();
-            if (empty($method)) {
-                $controllerClass->index();
-            } else {
-                $controllerClass->$method();
-            }
-
+      $page = "articles";
+      $controller = $segments[1];
+      $method = empty($segments[2]) ? "" : $segments[2];
+      if(!empty($controller) && $controller != "index.php") {
+        if (array_key_exists($controller, $pages)) {
+          $page = $controller;
         }
+        else {
+          http_response_code(404);
+          var_dump($segments);
+          echo "<br>" . $_SERVER["REQUEST_URI"];
+
+          echo "<br> NOT FOUND";
+          var_dump($_GET);
+          exit;
+        }
+      }
+
+      require $pages[$page]["path"];
+      $controllerClass = new $pages[$page]["class"]();
+      if (empty($method)) {
+        $controllerClass->index();
+      } else {
+        $controllerClass->$method();
+      }
+
     }
+  }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    
+
     define("BASE_URL", "http://localhost/blog/");
 
     define("VIEWS", "app/views/");
@@ -9,19 +9,3 @@
 
     require "app/controllers/controller.php";
     $articles = new Controller();
-
-
-
-//    $articles = new Articles();
-//    $articles ->getAll();
-//    $article = array("title" => "Is Friday", "body" => "Php for everyone!");
-//    $lastId = $articles->addArticle($article);
-//    echo "<pre>";
-//    var_dump($lastId);
-//    $articles ->getAll();
-//
-//    $comments = new Comments();
-//    $comments->getAll();
-//    $comment = array("article_id"=>"2", "email"=>"aaa@bbb.ccc", "body"=>"Hello!");
-//
-//    $comments->addComment($comment);
