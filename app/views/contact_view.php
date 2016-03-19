623bac0a7cc854d856578ea6ebff39aaecbbf855
calin1611 <calin1611@gmail.com>
2016-03-19 22:59:53 +0200

    Email sending not working
    Please log in via your web browser and then try again.
    Learn more at https://support.google.com/mail/answer/78754
    e19sm5144346wmd.1 - gsmtp

--- app/views/contact_view.php
+++ app/views/contact_view.php
@@ -17,22 +17,21 @@
 ?>
 <h1>Contact</h1>
 
+  <form action="http://localhost/blog/contact/sendEmail" method="post">
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" name="name" class="form-control" placeholder="Name" />
+    </div>
 
-    <form method="post">
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" name="name" class="form-control" placeholder="Name" />
-        </div>
+    <div class="form-group">
+      <label for="name">E-mail:</label>
+      <input type="text" name="email" class="form-control" placeholder="E-mail" />
+    </div>
 
-        <div class="form-group">
-            <label for="name">E-mail:</label>
-            <input type="text" name="email" class="form-control" placeholder="E-mail" />
-        </div>
+    <div class="form-group">
+      <label for="name">Message:</label>
+      <textarea name="message" class="form-control" placeholder="Message" ></textarea>
+    </div>
 
-        <div class="form-group">
-            <label for="name">Message:</label>
-            <input type="text" name="message" class="form-control" placeholder="Message" />
-        </div>
-
-        <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Send">
-    </form>
+    <input type="submit" name="submit" class="btn btn-primary btn-lg" value="Send">
+  </form>
